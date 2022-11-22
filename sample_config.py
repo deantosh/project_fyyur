21d6e6f18f717ee7d061894307a8108bb9b14ad4
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2022-11-22 16:14:14 +0300

    adding a sample config file

---
+++ sample_config.py
@@ -0,0 +1,24 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Database parameters
+DATABASE_NAME = ""
+username = ""
+password = ""
+url = "localhost:5432"
+
+# Create a database connection
+SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}/{}".format(
+    username,
+    password,
+    url,
+    DATABASE_NAME
+)
+
+# Reduce significant overhead
+SQLALCHEMY_TRACK_MODIFICATIONS = False
